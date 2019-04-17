eef43bb9f027ee4198c633ac572903cd52186a0b
Micah <mj2carlsbad@gmail.com>
2019-04-16 19:46:21 -0700

    -

--- app/src/main/java/com/example/miracostastaffdirectory/AllStaffListAdapter.java
+++ app/src/main/java/com/example/miracostastaffdirectory/AllStaffListAdapter.java
@@ -11,9 +11,6 @@ import android.widget.TextView;
 import java.util.List;
 import com.example.miracostastaffdirectory.Model.StaffMember;
 
-import static android.graphics.Color.GRAY;
-import static android.graphics.Color.YELLOW;
-
 
 public class AllStaffListAdapter extends ArrayAdapter<StaffMember> {
 
@@ -64,59 +61,6 @@ public class AllStaffListAdapter extends ArrayAdapter<StaffMember> {
         return v;
     }
 }
-
-
-
-
-/**
-public class AllStaffListAdapter extends ArrayAdapter<StaffMember> {
-
-    //declare member variables to store the params
-    private Context mContext;
-    private int mResourceId;
-    private List<StaffMember> mAllStaff;
-
-    //this constructor is being called  by MainActivity
-
-
-
-    public AllStaffListAdapter(@NonNull Context context, int resource, @NonNull List<StaffMember> staffList) {
-        super(context, resource, staffList);
-        mContext = context;
-        mResourceId = resource;
-        mAllStaff = staffList;
-
-    }
-
-
-    //in order to bridge the view (music_event_list_item) with model (MusicEvent) we override;
-    //crtl + o = override
-
-
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        // inflate custom layout with data from List<MusicEvents>
-
-        StaffMember focusedStaffMember = mAllStaff.get(position);
-
-        // manually inflate custom layout
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        //tell inflater to inflate music_event_list_item
-
-        View customView = inflater.inflate(mResourceId, null);
-
-        //fill parts of custom view
-
-        TextView listNameTextView = customView.findViewById(R.id.listNameTextView);
-
-        //put info in text views and image views
-        listNameTextView.setText(focusedStaffMember.getName());
-
-        return customView;
-}
-}
-*/
 
 
 
