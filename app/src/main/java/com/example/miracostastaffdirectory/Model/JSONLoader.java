124e1c477df1d4d2be5cc8a7079862324d9d5d47
Micah <mj2carlsbad@gmail.com>
2019-04-16 18:19:36 -0700

    added the real JSONLoader

--- app/src/main/java/com/example/miracostastaffdirectory/Model/JSONLoader.java
+++ app/src/main/java/com/example/miracostastaffdirectory/Model/JSONLoader.java
@@ -1,6 +1,7 @@
 package com.example.miracostastaffdirectory.Model;
 
 import android.content.Context;
+import android.text.InputType;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -27,49 +28,149 @@ public class JSONLoader {
     public static List<StaffMember> loadJSONFromAsset(Context context) throws IOException {
         List<StaffMember> allStaffList = new ArrayList<>();
         String json = null;
+        int it=0;
+        InputStream is = null;
 
+        ReadJSONLoop:
+        while (true) {
 
-        InputStream is = context.getAssets().open("AdimOfJustice.json");
-
-
-        int size = is.available();
-        byte[] buffer = new byte[size];
-        is.read(buffer);
-        is.close();
-        json = new String(buffer, "UTF-8");
-
-        try {
-            JSONObject jsonRootObject = new JSONObject(json);
-            JSONArray allCountriesJSON = jsonRootObject.getJSONArray("");
-
-            // Loop through all the countries in the JSON data, create a Country
-            int numCountries = allCountriesJSON.length();
-
-            // To be used in loop
-            JSONObject countryJSON;
-
-            StaffMember sMem = null;
-
-            for (int i = 0; i < numCountries; i++) {
-                countryJSON = allCountriesJSON.getJSONObject(i);
-
-              sMem = new StaffMember();
-                // Extract the name and region
-                sMem.setName(countryJSON.getString("Name"));
-                sMem.setTitle(countryJSON.getString("Title"));
-                sMem.setPhoneExt(countryJSON.getString("Phone"));
-                sMem.setLocation(countryJSON.getString("Location"));
-                sMem.setEmail(countryJSON.getString("Email"));
-
-                // Add object for each and add the object to the allCountriesList
-
-                allStaffList.add(sMem);
+            it++;
+            switch (it) {
+                case(1):
+                    is = context.getAssets().open("AdimOfJustice.json");
+                    break;
+                case(2):
+                    is = context.getAssets().open("ArtDepartment.json");
+                    break;
+                case(3):
+                    is = context.getAssets().open("AutomotiveTechnologyDepartment.json");
+                    break;
+                case(4):
+                    is = context.getAssets().open("BiologicalScienceDepartment.json");
+                    break;
+                case(5):
+                    is = context.getAssets().open("BiotechnologyDepartment.json");
+                    break;
+                case(6):
+                    is = context.getAssets().open("BusinessDepartment.json");
+                    break;
+                case(7):
+                    is = context.getAssets().open("CareerDepartment.json");
+                    break;
+                case(8):
+                    is = context.getAssets().open("ChemistryDepartment.json");
+                    break;
+                case(9):
+                    is = context.getAssets().open("ChildDevelopmentDepartment.json");
+                    break;
+                case(10):
+                    is = context.getAssets().open("CommunicationStudiesDepartment.json");
+                    break;
+                case(11):
+                    is = context.getAssets().open("CounselingDepartment.json");
+                    break;
+                case(12):
+                    is = context.getAssets().open("CSDepartment.json");
+                    break;
+                case(13):
+                    is = context.getAssets().open("CSITDepartment.json");
+                    break;
+                case(14):
+                    is = context.getAssets().open("DanceDepartment.json");
+                    break;
+                case(15):
+                    is = context.getAssets().open("DesignDepartment.json");
+                    break;
+                case(16):
+                    is = context.getAssets().open("EnglishDepartment.json");
+                    break;
+                case(17):
+                    is = context.getAssets().open("ESLDepartment.json");
+                    break;
+                case(18):
+                    is = context.getAssets().open("HistoryDepartment.json");
+                    break;
+                case(19):
+                    is = context.getAssets().open("HorticultureDepartment.json");
+                    break;
+                case(20):
+                    is = context.getAssets().open("IMTDDepartment.json");
+                    break;
+                case(21):
+                    is = context.getAssets().open("InternationalLanguagesDepartment.json");
+                    break;
+                case(22):
+                    is = context.getAssets().open("KinesiologyDepartment.json");
+                    break;
+                case(23):
+                    is = context.getAssets().open("LibraryScienceDepartment.json");
+                    break;
+                case(24):
+                    is = context.getAssets().open("MathDepartment.json");
+                    break;
+                case(25):
+                    is = context.getAssets().open("MusicDepartment.json");
+                    break;
+                case(26):
+                    is = context.getAssets().open("NursingDepartment.json");
+                    break;
+                case(27):
+                    is = context.getAssets().open("PhilosophyAndReligiousDepartment.json");
+                    break;
+                case(28):
+                    is = context.getAssets().open("PhysicalScienceDepartment.json");
+                    break;
+                case(29):
+                    is = context.getAssets().open("PsychologyDepartment.json");
+                    break;
+                case(30):
+                    is = context.getAssets().open("SociologyDepartment.json");
+                    break;
+                case(31):
+                    is = context.getAssets().open("TheatreAndFilmDepartment.json");
+                    break;
+                default:
+                    break ReadJSONLoop;
             }
 
 
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
 
-        } catch (JSONException e) {
-            Log.e("Flag Quiz", e.getMessage());
+            try {
+                JSONObject jsonRootObject = new JSONObject(json);
+                JSONArray allCountriesJSON = jsonRootObject.getJSONArray("");
+
+                // Loop through all the countries in the JSON data, create a Country
+                int numCountries = allCountriesJSON.length();
+
+                // To be used in loop
+                JSONObject countryJSON;
+                String name, title, phoneExt, location, email;
+                StaffMember sm = null;
+
+                for (int i = 0; i < numCountries; i++) {
+                    countryJSON = allCountriesJSON.getJSONObject(i);
+
+                    // Extract the name and region
+                    name = countryJSON.getString("Name");
+                    title = countryJSON.getString("Title");
+                    phoneExt = countryJSON.getString("Phone");
+                    location = countryJSON.getString("Location");
+                    email = countryJSON.getString("Email");
+
+                    // Add object for each and add the object to the allCountriesList
+                    sm = new StaffMember(name, title, phoneExt, location, email);
+                    allStaffList.add(sm);
+                }
+
+
+            } catch (JSONException e) {
+                Log.e("Flag Quiz", e.getMessage());
+            }
         }
 
         return allStaffList;
