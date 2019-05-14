3e90f1a785c26fb0ff13397e5ed1087268ff23df
micah <mj2carlsbad@gmail.com>
2019-05-14 15:28:05 -0700

    singleDeptStaff working but getting ALL staff unintentionally

--- app/src/main/java/com/example/miracostastaffdirectory/SingleDeptStaff.java
+++ app/src/main/java/com/example/miracostastaffdirectory/SingleDeptStaff.java
@@ -14,6 +14,8 @@ import android.widget.ListView;
 import com.example.miracostastaffdirectory.Model.StaffMember;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class SingleDeptStaff extends AppCompatActivity {
 
@@ -31,11 +33,17 @@ public class SingleDeptStaff extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_staff);
 
+        // og arrayList
         staffInDept = new ArrayList<>();
 
-        Intent fromDepartmentsAct = getIntent();
-        String deptToLoad = fromDepartmentsAct.getStringExtra("department");
-        allStaff = fromDepartmentsAct.getParcelableArrayListExtra("allStaff");
+        Intent from = getIntent();
+        String deptToLoad = from.getStringExtra("department");
+        // this will get an arrayList regardless of which activity it comes from since they have the same key
+        allStaff = from.getParcelableArrayListExtra("allStaff");
+        prevScroll = from.getIntExtra("prevScroll", -1);
+        prevSearch = from.getStringExtra("prevSearch");
+        if (prevSearch==null)
+            prevSearch="";
 
 
         // we will assign this string based on what department we're passed from Departments.java
@@ -52,180 +60,190 @@ public class SingleDeptStaff extends AppCompatActivity {
         boolean thereAreThreeFuckingWords = false;
         String thirdSearchKey="";
 
+        {
+            switch (deptToLoad) {
 
-        switch (deptToLoad) {
-
-            case ("Administration Of Justice Department"):
-                searchKey = "Justice";
-                break;
-            case ("Art Department"):
-                searchKey = "Art";
-                break;
-            case ("Automotive Technology Department"):
-                searchKey = "Automotive";
-                break;
-            case ("Biological Science Department"):
-                searchKey = "Biology";
-                break;
-            case ("Biotechnology Department"):
-                thereAreTwoWordsToSearchFor = true;
-                searchKey = "Biotechnology";
-                secondSearchKey = "Biomanufacturing";
-                break;
-            case ("Business Department"):
-                thereAreThreeFuckingWords = true;
-                searchKey = "Business";
-                secondSearchKey = "Accounting";
-                thirdSearchKey = "Hospitality";
-                break;
-            case ("Career Department"):
-                searchKey = "Career";
-                break;
-            case ("Chemistry Department"):
-                searchKey = "Chemistry";
-                break;
-            case ("Child Development Department"):
-                searchKey = "Child";
-                break;
-            case ("Communication Studies Department"):
-                searchKey = "Communication";
-                break;
-            case ("Counseling Department"):
-                searchKey = "Counsel";
-                break;
-            case ("Computer Science Department"):
-                searchKey = "Computer";
-                break;
-            case ("CSIT Department"):
-                searchKey = "CSIT";
-                break;
-            case ("Dance Department"):
-                searchKey = "Dance";
-                break;
-            case ("Design Department"):
-                searchKey = "Design";
-                break;
-            case ("English Department"):
-                searchKey = "English";
-                break;
-            case ("ESLD Department"):
-                searchKey = "ESL";
-                break;
-            case ("History Department"):
-                searchKey = "History";
-                break;
-            case ("Horticulture Department"):
-                searchKey = "Horticulture";
-                break;
-            case ("IMTD Department"):
-                searchKey = "Media Arts";
-                break;
-            case ("International Languages Department"):
-                searchKey = "International Languages";
-                break;
-            case ("Kinesiology Department"):
-                thereAreThreeFuckingWords = true;
-                searchKey = "Kinesiology";
-                secondSearchKey = "Massage";
-                thirdSearchKey = "Nutrition";
-                break;
-            case ("Library Science Department"):
-                searchKey = "Librar";
-                break;
-            case ("Math Department"):
-                searchKey = "Math";
-                break;
-            case ("Music Department"):
-                searchKey = "Music";
-                break;
-            case ("Nursing Department"):
-                thereAreThreeFuckingWords = true;
-                searchKey = "Nursing";
-                secondSearchKey = "Surgical";
-                thirdSearchKey = "Pharmacology";
-                break;
-            case ("Philosophy And Religious Department"):
-                thereAreTwoWordsToSearchFor = true;
-                searchKey = "Philosophy";
-                secondSearchKey = "Religious";
-                break;
-            case ("Physical Science Department"):
-                thereAreThreeFuckingWords = true;
-                searchKey = "Physics";
-                secondSearchKey = "Oceanography";
-                thirdSearchKey = "Astronomy";
-                break;
-            case ("Psychology Department"):
-                searchKey = "Psychology";
-                break;
-            case ("Sociology Department"):
-                searchKey = "Sociology";
-                break;
-            case ("Theatre And Film Department"):
-                thereAreThreeFuckingWords = true;
-                searchKey = "Film";
-                secondSearchKey = "Theatre";
-                thirdSearchKey = "Drama";
-                break;
-            default:
-                System.out.println("ERROR CODE: 000000x5556732222x90");
-                System.out.println("Something went terribly wrong, we didn't get a matching string " +
-                        "\nfrom the Departments Activity in the Intent");
-                System.exit(0);
-        }
-
-        // if we are searching for three words
-        if (thereAreThreeFuckingWords) {
-
-            // loop through all staff
-            for (int i=0; i < allStaff.size(); i++) {
-
-                // if it contains one of the three search keys
-                if (allStaff.get(i).getTitle().contains(searchKey)
-                        || allStaff.get(i).getTitle().contains(secondSearchKey)
-                        || allStaff.get(i).getTitle().contains(thirdSearchKey)) {
-
-                    // add it to the list
-                    staffInDept.add(allStaff.get(i));
-                }
-            }
-        }
-
-        // if we are searching for two words
-        if (thereAreTwoWordsToSearchFor) {
-
-            // loop through all staff
-            for (int i =0; i < allStaff.size(); i++) {
-
-                //if the title contains one of the two search words
-                if (allStaff.get(i).getTitle().contains(searchKey)
-                    ||  allStaff.get(i).getTitle().contains(secondSearchKey)) {
-
-                    // add it to the list
-                    staffInDept.add(allStaff.get(i));
-                }
-
+                case ("Administration Of Justice Department"):
+                    searchKey = "Justice";
+                    break;
+                case ("Art Department"):
+                    searchKey = "Art";
+                    break;
+                case ("Automotive Technology Department"):
+                    searchKey = "Automotive";
+                    break;
+                case ("Biological Science Department"):
+                    searchKey = "Biology";
+                    break;
+                case ("Biotechnology Department"):
+                    thereAreTwoWordsToSearchFor = true;
+                    searchKey = "Biotechnology";
+                    secondSearchKey = "Biomanufacturing";
+                    break;
+                case ("Business Department"):
+                    thereAreThreeFuckingWords = true;
+                    searchKey = "Business";
+                    secondSearchKey = "Accounting";
+                    thirdSearchKey = "Hospitality";
+                    break;
+                case ("Career Department"):
+                    searchKey = "Career";
+                    break;
+                case ("Chemistry Department"):
+                    searchKey = "Chemistry";
+                    break;
+                case ("Child Development Department"):
+                    searchKey = "Child";
+                    break;
+                case ("Communication Studies Department"):
+                    searchKey = "Communication";
+                    break;
+                case ("Counseling Department"):
+                    searchKey = "Counsel";
+                    break;
+                case ("Computer Science Department"):
+                    searchKey = "Computer";
+                    break;
+                case ("CSIT Department"):
+                    searchKey = "CSIT";
+                    break;
+                case ("Dance Department"):
+                    searchKey = "Dance";
+                    break;
+                case ("Design Department"):
+                    searchKey = "Design";
+                    break;
+                case ("English Department"):
+                    searchKey = "English";
+                    break;
+                case ("ESLD Department"):
+                    searchKey = "ESL";
+                    break;
+                case ("History Department"):
+                    searchKey = "History";
+                    break;
+                case ("Horticulture Department"):
+                    searchKey = "Horticulture";
+                    break;
+                case ("IMTD Department"):
+                    searchKey = "Media Arts";
+                    break;
+                case ("International Languages Department"):
+                    searchKey = "International Languages";
+                    break;
+                case ("Kinesiology Department"):
+                    thereAreThreeFuckingWords = true;
+                    searchKey = "Kinesiology";
+                    secondSearchKey = "Massage";
+                    thirdSearchKey = "Nutrition";
+                    break;
+                case ("Library Science Department"):
+                    searchKey = "Librar";
+                    break;
+                case ("Math Department"):
+                    searchKey = "Math";
+                    break;
+                case ("Music Department"):
+                    searchKey = "Music";
+                    break;
+                case ("Nursing Department"):
+                    thereAreThreeFuckingWords = true;
+                    searchKey = "Nursing";
+                    secondSearchKey = "Surgical";
+                    thirdSearchKey = "Pharmacology";
+                    break;
+                case ("Philosophy And Religious Department"):
+                    thereAreTwoWordsToSearchFor = true;
+                    searchKey = "Philosophy";
+                    secondSearchKey = "Religious";
+                    break;
+                case ("Physical Science Department"):
+                    thereAreThreeFuckingWords = true;
+                    searchKey = "Physics";
+                    secondSearchKey = "Oceanography";
+                    thirdSearchKey = "Astronomy";
+                    break;
+                case ("Psychology Department"):
+                    searchKey = "Psychology";
+                    break;
+                case ("Sociology Department"):
+                    searchKey = "Sociology";
+                    break;
+                case ("Theatre And Film Department"):
+                    thereAreThreeFuckingWords = true;
+                    searchKey = "Film";
+                    secondSearchKey = "Theatre";
+                    thirdSearchKey = "Drama";
+                    break;
+                default:
+                    System.out.println("ERROR CODE: 000000x5556732222x90");
+                    System.out.println("Something went terribly wrong, we didn't get a matching string " +
+                            "\nfrom the Departments Activity in the Intent");
+                    System.exit(0);
             }
 
-        }
+            // if we are searching for three words
+            if (thereAreThreeFuckingWords) {
 
-        // else we are only searching for one search keyword
-        else {
-            for (int i =0; i < allStaff.size(); i++) {
-                if (allStaff.get(i).getTitle().contains(searchKey)) {
-                    staffInDept.add(allStaff.get(i));
+                // loop through all staff
+                for (int i = 0; i < allStaff.size(); i++) {
+
+                    // if it contains one of the three search keys
+                    if (allStaff.get(i).getTitle().contains(searchKey)
+                            || allStaff.get(i).getTitle().contains(secondSearchKey)
+                            || allStaff.get(i).getTitle().contains(thirdSearchKey)) {
+
+                        // add it to the list
+                        staffInDept.add(allStaff.get(i));
+                    }
+                }
+            }
+
+            // if we are searching for two words
+            if (thereAreTwoWordsToSearchFor) {
+
+                // loop through all staff
+                for (int i = 0; i < allStaff.size(); i++) {
+
+                    //if the title contains one of the two search words
+                    if (allStaff.get(i).getTitle().contains(searchKey)
+                            || allStaff.get(i).getTitle().contains(secondSearchKey)) {
+
+                        // add it to the list
+                        staffInDept.add(allStaff.get(i));
+                    }
+
+                }
+
+            }
+
+            // else we are only searching for one search keyword
+            else {
+                for (int i = 0; i < allStaff.size(); i++) {
+                    if (allStaff.get(i).getTitle().contains(searchKey)) {
+                        staffInDept.add(allStaff.get(i));
+                    }
                 }
             }
         }
 
         // allStaff should be populated
         // make a filtered list for searching
+        Log.i("MCC Staff Dir", "allStaff size = " + allStaff.size());
+        Collections.sort(allStaff, new Comparator<StaffMember>() {
+            public int compare(StaffMember s1, StaffMember s2) {
+                return s1.getName().compareTo(s2.getName());
+            }
+        });
         filteredStaff = new ArrayList<>();
         filteredStaff.addAll(allStaff);
 
+        // create adapter, define list view, and set them
         adapter = new StaffListAdapter(this, R.layout.simple_one_text_line_item, filteredStaff, prevScroll);
+        staffListView = findViewById(R.id.AllStaffListView);
         staffListView.setAdapter(adapter);
 
+        // highlight previous thing
         staffListView.setSelection(prevScroll-6);
 
         AdapterView.OnItemClickListener onItemClickListener = (new AdapterView.OnItemClickListener() {
