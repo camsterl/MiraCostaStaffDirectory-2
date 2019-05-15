d5a20767b7c6e9523afd71e99d616ad03f1e68cf
micah <mj2carlsbad@gmail.com>
2019-05-14 17:15:35 -0700

    search added to Department

--- app/src/main/java/com/example/miracostastaffdirectory/Departments.java
+++ app/src/main/java/com/example/miracostastaffdirectory/Departments.java
@@ -3,6 +3,8 @@ package com.example.miracostastaffdirectory;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
