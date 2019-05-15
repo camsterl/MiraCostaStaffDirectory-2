b597f42370bfb6eb58713caac2d2e9909756d8f0
1200W040 <cameronkerr@iinet.net.au>
2019-05-14 18:33:26 -0700

    splash screen 2.0 8 milliseconds

--- app/src/main/java/com/example/miracostastaffdirectory/SplashActivity.java
+++ app/src/main/java/com/example/miracostastaffdirectory/SplashActivity.java
@@ -36,7 +36,7 @@ public class SplashActivity extends AppCompatActivity {
         };
 
         Timer timer = new Timer();
-        timer.schedule(task,1500);
+        timer.schedule(task,800);
     }
 }
 
